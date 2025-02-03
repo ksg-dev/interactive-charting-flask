174e3ca28d51b3660fc6b917bfde296712197686
ksg-dev <ksg.dev.data@gmail.com>
2025-02-03 17:38:44 -0600

    chartjs testing

---
+++ app/routes.py
@@ -0,0 +1,86 @@
+from app import app
+from flask import Flask, render_template
+from bokeh.embed import components
+from bokeh.plotting import figure
+
+import random
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/bokeh-multi')
+def bokeh_multi():
+    # First chart - scatter plot
+    p1 = figure(height=350, sizing_mode="stretch_width")
+    p1.circle(
+        [i for i in range(10)],
+        [random.randint(1,50) for j in range(10)],
+        size=20,
+        color="navy",
+        alpha=0.5
+    )
+
+    # Second chart - line plot
+    language = ["Python", "JavaScript", "C++", "C#", "Java", "Golang"]
+    popularity = [85, 91, 63, 58, 80, 77]
+
+    p2 = figure(
+        x_range=language,
+        height=350,
+        title="Popularity",
+    )
+    p2.vbar(x=language, top=popularity, width=0.5)
+    p2.xgrid.grid_line_color = None
+    p2.y_range.start = 0
+
+    # Third chart - line plot
+    p3 = figure(height=350, sizing_mode="stretch_width")
+    p3.line(
+        [i for i in range(10)],
+        [random.randint(1,50) for j in range(10)],
+        line_width=2,
+        color="olive",
+        alpha=0.5
+    )
+
+    script1, div1 = components(p1)
+    script2, div2 = components(p2)
+    script3, div3 = components(p3)
+
+    # Return all charts to HTML template
+    return render_template(
+        template_name_or_list='bokeh-multi.html',
+        script=[script1, script2, script3],
+        div=[div1, div2, div3],
+    )
+
+@app.route('/chartjs')
+def chartjs():
+
+    # Define plot data
+    labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+
+    data = [0, 10, 15, 8, 22, 18, 25, 10, 15, 8, 22, 18, 25]
+
+    # Return components to HTML template
+    return render_template(
+        template_name_or_list='chart-js.html',
+        data=data,
+        labels=labels,
+    )
